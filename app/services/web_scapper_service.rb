45c3bb7d23b2bb8742c5e4978606ac7cbc273bd3
David Jarillo <vjdavid621@gmail.com>
2021-01-10 21:47:35 -0800

    adding logic to retrieve the url page to save and save title of the webpage

--- app/services/web_scapper_service.rb
+++ app/services/web_scapper_service.rb
@@ -1,10 +1,19 @@
 class WebScapperService
 
-  def perform(url)
-    parse_title_tag(url)
+  def perform(*params)
+    parse_title_tag(params[:url])
   end
 
   private
+
+  def stored_url
+    stored_url ||= ShortenedUrl.find_by(path_url: params[:path_url])
+  end
+
+  def save_title_page
+    stored_url.title = parse_title_tag(stored_url.real_url)
+    stored_url.save
+  end
 
   def parse_html_body(url)
     HTTParty.get(url).body
